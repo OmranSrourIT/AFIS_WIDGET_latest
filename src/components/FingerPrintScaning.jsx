dc410b1b01683fdac77c593e454875f8b90de213
omran.srour@polaris-tek.com <omran.srour@polaris-tek.com>
2022-12-25 17:30:42 +0300

    latest 12_25_2022 end code

---
+++ src/components/FingerPrintScaning.jsx
@@ -0,0 +1,17 @@
+import { Component, createElement } from "react";
+
+
+
+export class FingerPrintScaning extends Component {
+
+    render() {
+        return (
+            <div class="scanWidget">
+                <div class="fingerprintWidget">
+                    <p class="hh"></p>
+                </div> 
+                {/* <h3 class="h3widget">إلتقـاط البصمة</h3> */}
+            </div>
+        )
+    }
+}
